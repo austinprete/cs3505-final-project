2a5f459e21330e726a4e2c4ae3023a75fa7d6142
Cole Perschon <ColePerschon38@gmail.com>
2018-04-02 21:22:22 -0600

    push testing.

--- SpreadsheetClient/SpreadsheetGUI/Program.cs
+++ SpreadsheetClient/SpreadsheetGUI/Program.cs
@@ -17,6 +17,7 @@ namespace SpreadsheetGUI
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new SpreadsheetForm());
+            //test
         }
     }
 }
