f754c82fea972fc34caa003d0fee88445ef3d07b
Cole Perschon <ColePerschon38@gmail.com>
2018-04-02 22:39:44 -0600

    UI updates.

--- SpreadsheetClient/MenuGUI/PortalForm.cs
+++ SpreadsheetClient/MenuGUI/PortalForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace MenuGUI
 {
-    public partial class Form1 : Form
+    public partial class PortalForm : Form
     {
-        public Form1()
+        public PortalForm()
         {
             InitializeComponent();
         }
