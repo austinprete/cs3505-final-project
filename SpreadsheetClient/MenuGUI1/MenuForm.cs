928bc8906db05ae9de0b59e6f12d7f6d59cf7bdd
matbeseris <mathewbeseris@gmail.com>
2018-04-12 14:50:14 -0600

    MErge?

--- SpreadsheetClient/MenuGUI1/MenuForm.cs
+++ SpreadsheetClient/MenuGUI1/MenuForm.cs
@@ -78,9 +78,10 @@ namespace MenuGUI
 
         private void MenuForm_FormClosing(object sender, FormClosingEventArgs e)
         {
-            if (!LoggedOut)
+            if (socket_state.theSocket.Connected)
             {
                 socket_state.theSocket.Disconnect(true);
+                socket_state.theSocket.Close();
                 Application.Exit();
             }
                 
