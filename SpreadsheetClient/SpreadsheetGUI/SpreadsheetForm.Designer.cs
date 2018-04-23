c5531d08099f17f3cd9663af136e84951f947d0d
Cole Perschon <ColePerschon38@gmail.com>
2018-04-22 22:26:55 -0600

    Removed File Tab...

--- SpreadsheetClient/SpreadsheetGUI/SpreadsheetForm.Designer.cs
+++ SpreadsheetClient/SpreadsheetGUI/SpreadsheetForm.Designer.cs
@@ -28,78 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.undoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.revertToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.panel1 = new System.Windows.Forms.Panel();
             this.ErrorMsgBox = new System.Windows.Forms.TextBox();
             this.CellContentsTextBox = new System.Windows.Forms.TextBox();
-            this.spreadsheetPanel1 = new SS.SpreadsheetPanel();
             this.CellValueTextBox = new System.Windows.Forms.TextBox();
             this.CellNameTextbox = new System.Windows.Forms.TextBox();
-            this.menuStrip1.SuspendLayout();
+            this.spreadsheetPanel1 = new SS.SpreadsheetPanel();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.undoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.revertToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.panel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // fileToolStripMenuItem
+            // panel1
             // 
-            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.newToolStripMenuItem,
-            this.saveToolStripMenuItem,
-            this.openToolStripMenuItem,
-            this.closeToolStripMenuItem});
-            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(64, 36);
-            this.fileToolStripMenuItem.Text = "File";
+            this.panel1.Controls.Add(this.ErrorMsgBox);
+            this.panel1.Controls.Add(this.CellContentsTextBox);
+            this.panel1.Controls.Add(this.spreadsheetPanel1);
+            this.panel1.Controls.Add(this.CellValueTextBox);
+            this.panel1.Controls.Add(this.CellNameTextbox);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 40);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1611, 865);
+            this.panel1.TabIndex = 2;
             // 
-            // newToolStripMenuItem
+            // ErrorMsgBox
             // 
-            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
-            this.newToolStripMenuItem.Size = new System.Drawing.Size(173, 38);
-            this.newToolStripMenuItem.Text = "New";
-            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            this.ErrorMsgBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ErrorMsgBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.ErrorMsgBox.Enabled = false;
+            this.ErrorMsgBox.Location = new System.Drawing.Point(11, 62);
+            this.ErrorMsgBox.Margin = new System.Windows.Forms.Padding(4);
+            this.ErrorMsgBox.Name = "ErrorMsgBox";
+            this.ErrorMsgBox.ReadOnly = true;
+            this.ErrorMsgBox.Size = new System.Drawing.Size(1589, 31);
+            this.ErrorMsgBox.TabIndex = 5;
+            this.ErrorMsgBox.Visible = false;
             // 
-            // saveToolStripMenuItem
+            // CellContentsTextBox
             // 
-            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(173, 38);
-            this.saveToolStripMenuItem.Text = "Save";
-            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            this.CellContentsTextBox.AcceptsReturn = true;
+            this.CellContentsTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.CellContentsTextBox.Enabled = false;
+            this.CellContentsTextBox.Location = new System.Drawing.Point(740, 12);
+            this.CellContentsTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.CellContentsTextBox.Name = "CellContentsTextBox";
+            this.CellContentsTextBox.Size = new System.Drawing.Size(677, 31);
+            this.CellContentsTextBox.TabIndex = 3;
+            this.CellContentsTextBox.Visible = false;
             // 
-            // openToolStripMenuItem
+            // CellValueTextBox
             // 
-            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(173, 38);
-            this.openToolStripMenuItem.Text = "Open";
-            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            this.CellValueTextBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.CellValueTextBox.Enabled = false;
+            this.CellValueTextBox.Location = new System.Drawing.Point(104, 12);
+            this.CellValueTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.CellValueTextBox.Name = "CellValueTextBox";
+            this.CellValueTextBox.ReadOnly = true;
+            this.CellValueTextBox.Size = new System.Drawing.Size(623, 31);
+            this.CellValueTextBox.TabIndex = 1;
             // 
-            // closeToolStripMenuItem
+            // CellNameTextbox
             // 
-            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
-            this.closeToolStripMenuItem.Size = new System.Drawing.Size(173, 38);
-            this.closeToolStripMenuItem.Text = "Close";
-            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            this.CellNameTextbox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.CellNameTextbox.Enabled = false;
+            this.CellNameTextbox.Location = new System.Drawing.Point(11, 12);
+            this.CellNameTextbox.Margin = new System.Windows.Forms.Padding(4);
+            this.CellNameTextbox.Name = "CellNameTextbox";
+            this.CellNameTextbox.ReadOnly = true;
+            this.CellNameTextbox.Size = new System.Drawing.Size(72, 31);
+            this.CellNameTextbox.TabIndex = 0;
             // 
-            // menuStrip1
+            // spreadsheetPanel1
             // 
-            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(36, 36);
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fileToolStripMenuItem,
-            this.helpToolStripMenuItem,
-            this.undoToolStripMenuItem,
-            this.revertToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(1611, 40);
-            this.menuStrip1.TabIndex = 1;
-            this.menuStrip1.Text = "menuStrip1";
+            this.spreadsheetPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.spreadsheetPanel1.Location = new System.Drawing.Point(11, 112);
+            this.spreadsheetPanel1.Margin = new System.Windows.Forms.Padding(5);
+            this.spreadsheetPanel1.Name = "spreadsheetPanel1";
+            this.spreadsheetPanel1.Size = new System.Drawing.Size(1600, 753);
+            this.spreadsheetPanel1.TabIndex = 2;
+            this.spreadsheetPanel1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.spreadsheetPanel1_KeyPress);
             // 
             // helpToolStripMenuItem
             // 
@@ -122,81 +139,19 @@
             this.revertToolStripMenuItem.Text = "Revert";
             this.revertToolStripMenuItem.Click += new System.EventHandler(this.revertToolStripMenuItem_Click);
             // 
-            // panel1
+            // menuStrip1
             // 
-            this.panel1.Controls.Add(this.ErrorMsgBox);
-            this.panel1.Controls.Add(this.CellContentsTextBox);
-            this.panel1.Controls.Add(this.spreadsheetPanel1);
-            this.panel1.Controls.Add(this.CellValueTextBox);
-            this.panel1.Controls.Add(this.CellNameTextbox);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(0, 40);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1611, 865);
-            this.panel1.TabIndex = 2;
-            // 
-            // ErrorMsgBox
-            // 
-            this.ErrorMsgBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.ErrorMsgBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
-            this.ErrorMsgBox.Enabled = false;
-            this.ErrorMsgBox.Location = new System.Drawing.Point(11, 62);
-            this.ErrorMsgBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.ErrorMsgBox.Name = "ErrorMsgBox";
-            this.ErrorMsgBox.ReadOnly = true;
-            this.ErrorMsgBox.Size = new System.Drawing.Size(1589, 31);
-            this.ErrorMsgBox.TabIndex = 5;
-            this.ErrorMsgBox.Visible = false;
-            // 
-            // CellContentsTextBox
-            // 
-            this.CellContentsTextBox.AcceptsReturn = true;
-            this.CellContentsTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.CellContentsTextBox.Enabled = false;
-            this.CellContentsTextBox.Location = new System.Drawing.Point(740, 12);
-            this.CellContentsTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.CellContentsTextBox.Name = "CellContentsTextBox";
-            this.CellContentsTextBox.Size = new System.Drawing.Size(677, 31);
-            this.CellContentsTextBox.TabIndex = 3;
-            this.CellContentsTextBox.Visible = false;
-            // 
-            // spreadsheetPanel1
-            // 
-            this.spreadsheetPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.spreadsheetPanel1.Location = new System.Drawing.Point(11, 112);
-            this.spreadsheetPanel1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
-            this.spreadsheetPanel1.Name = "spreadsheetPanel1";
-            this.spreadsheetPanel1.Size = new System.Drawing.Size(1600, 753);
-            this.spreadsheetPanel1.TabIndex = 2;
-            this.spreadsheetPanel1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.spreadsheetPanel1_KeyPress);
-            // 
-            // CellValueTextBox
-            // 
-            this.CellValueTextBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.CellValueTextBox.Enabled = false;
-            this.CellValueTextBox.Location = new System.Drawing.Point(104, 12);
-            this.CellValueTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.CellValueTextBox.Name = "CellValueTextBox";
-            this.CellValueTextBox.ReadOnly = true;
-            this.CellValueTextBox.Size = new System.Drawing.Size(623, 31);
-            this.CellValueTextBox.TabIndex = 1;
-            // 
-            // CellNameTextbox
-            // 
-            this.CellNameTextbox.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.CellNameTextbox.Enabled = false;
-            this.CellNameTextbox.Location = new System.Drawing.Point(11, 12);
-            this.CellNameTextbox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.CellNameTextbox.Name = "CellNameTextbox";
-            this.CellNameTextbox.ReadOnly = true;
-            this.CellNameTextbox.Size = new System.Drawing.Size(72, 31);
-            this.CellNameTextbox.TabIndex = 0;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(36, 36);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem,
+            this.undoToolStripMenuItem,
+            this.revertToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(1611, 40);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
             // 
             // SpreadsheetForm
             // 
@@ -206,37 +161,31 @@
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.menuStrip1);
             this.KeyPreview = true;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "SpreadsheetForm";
             this.Text = "Spreadsheet Editor";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.SpreadsheetForm_FormClosed);
             this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.SpreadsheetForm_KeyPress);
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
-        private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.TextBox CellValueTextBox;
         private System.Windows.Forms.TextBox CellNameTextbox;
         private SS.SpreadsheetPanel spreadsheetPanel1;
         private System.Windows.Forms.TextBox CellContentsTextBox;
         private System.Windows.Forms.TextBox ErrorMsgBox;
-        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem undoToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem revertToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
     }
 }
 
